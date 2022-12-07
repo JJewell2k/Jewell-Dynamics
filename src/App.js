82cec92269b84d84f82e8905161dd60bae6dcaf4
JJewell2k <90526983+JJewell2k@users.noreply.github.com>
2022-12-07 17:15:59 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Welcome To the FUTURE!!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jewell-dynamics.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MINT NFT
         </a>
       </header>
     </div>
