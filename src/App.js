dd6ec5aa2e2c95f5990b787cfb4aff2085bbc49f
JJewell2k <90526983+JJewell2k@users.noreply.github.com>
2022-12-07 23:08:24 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,23 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Welcome To the FUTURE!!
-        </p>
-        <a
-          className="App-link"
-          href="https://jewell-dynamics.io"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          MINT NFT
-        </a>
-      </header>
-    </div>
-  );
+    <div
 window.onload = function () {
   Particles.init({
     selector: ".background"
@@ -147,5 +131,6 @@ class NavigationPage {
 }
 
 new NavigationPage();
+  );
 
 export default App;
